1d719bc542720c8cd279a71ed07c47ddd4a9780a
alex1431999 <alex@baserow.io>
2023-07-20 10:10:34 +0200

    Add toTipTapVisitor tests
    Also add test cases that are shared between the 2 tests

--- web-frontend/test/unit/core/formula/fromTipTapVisitor.spec.js
+++ web-frontend/test/unit/core/formula/fromTipTapVisitor.spec.js
@@ -1,6 +1,7 @@
 import { RuntimeFunctionCollection } from '@baserow/modules/core/functionCollection'
 import { TestApp } from '@baserow/test/helpers/testApp'
 import { FromTipTapVisitor } from '@baserow/modules/core/formula/fromTipTapVisitor'
+import testCases from '@baserow_test_cases/tip_tap_visitor_cases.json'
 
 describe('fromTipTapVisitor', () => {
   let testApp = null
@@ -8,60 +9,13 @@ describe('fromTipTapVisitor', () => {
     testApp = new TestApp()
   })
 
-  const testCases = [
-    {
-      content: [],
-      expected: '',
-    },
-    {
-      content: [{ type: 'text', text: 'hello' }],
-      expected: "'hello'",
-    },
-    {
-      content: [
-        { type: 'text', text: 'hello' },
-        { type: 'text', text: 'there' },
-      ],
-      expected: "concat('hello', 'there')",
-    },
-    {
-      content: [
-        { type: 'text', text: 'hello' },
-        { type: 'text', text: 'there' },
-        { type: 'text', text: 'friend :)' },
-      ],
-      expected: "concat(concat('hello', 'there'), 'friend :)')",
-    },
-    {
-      content: [
-        {
-          type: 'get-formula-component',
-          attrs: { path: 'data_source.hello.there' },
-        },
-      ],
-      expected: "get('data_source.hello.there')",
-    },
-    {
-      content: [
-        {
-          type: 'get-formula-component',
-          attrs: { path: 'data_source.hello.there' },
-        },
-        { type: 'text', text: 'friend :)' },
-      ],
-      expected: "concat(get('data_source.hello.there'), 'friend :)')",
-    },
-  ]
-
-  testCases.forEach((testCase) => {
+  testCases.forEach(({ formula, content }) => {
     it('should return the expected formula', () => {
       const functionCollection = new RuntimeFunctionCollection(
         testApp.store.$registry
       )
-      const result = new FromTipTapVisitor(functionCollection).visit(
-        testCase.content
-      )
-      expect(result).toEqual(testCase.expected)
+      const result = new FromTipTapVisitor(functionCollection).visit(content)
+      expect(result).toEqual(formula)
     })
   })
 })
