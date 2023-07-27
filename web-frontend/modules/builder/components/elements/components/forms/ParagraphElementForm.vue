de5973e1ba1fe2a72e0951040581e78026c117d9
alex1431999 <alex@baserow.io>
2023-07-27 13:23:29 +0200

    Remove input field from paragraph form

--- web-frontend/modules/builder/components/elements/components/forms/ParagraphElementForm.vue
+++ web-frontend/modules/builder/components/elements/components/forms/ParagraphElementForm.vue
@@ -5,11 +5,13 @@
         {{ $t('paragraphElementForm.textTitle') }}
       </label>
       <div class="control__elements">
-        <FormulaInputField
+        <textarea
           v-model="values.value"
           :placeholder="$t('elementForms.textInputPlaceholder')"
           class="input paragraph-element-form__value"
-        ></FormulaInputField>
+          type="text"
+          rows="12"
+        ></textarea>
       </div>
     </FormElement>
   </form>
@@ -17,11 +19,9 @@
 
 <script>
 import form from '@baserow/modules/core/mixins/form'
-import FormulaInputField from '@baserow/modules/core/components/formula/FormulaInputField'
 
 export default {
   name: 'ParagraphElementForm',
-  components: { FormulaInputField },
   mixins: [form],
   props: {},
   data() {
