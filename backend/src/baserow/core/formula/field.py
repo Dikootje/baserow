cd5c28dacbf341f1606ded2a75b7fa73bb56399b
Jeremie Pardou <jeremie@baserow.io>
2023-07-28 11:11:58 +0200

    Add doc

--- backend/src/baserow/core/formula/field.py
+++ backend/src/baserow/core/formula/field.py
@@ -3,5 +3,9 @@ from django.db import models
 
 class FormulaField(models.TextField):
     """
-    A formula field.
+    A formula field contains the text value of a runtime formula like:
+    - concat("test:", get("page_parameter.id"), "-")
+    - get("data_source.Product.id")
+
+    For now it's just a text field but we can add layer of validation later.
     """
