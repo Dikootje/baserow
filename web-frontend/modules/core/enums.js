eec14bcde8782e537120b04543b1f5fd1ddeccc6
Davide Silvestri <davide@baserow.io>
2023-08-08 23:49:51 +0200

    Add missing comment to keep options in sync

--- web-frontend/modules/core/enums.js
+++ web-frontend/modules/core/enums.js
@@ -1,4 +1,7 @@
 export const IMAGE_FILE_TYPES = ['image/jpeg', 'image/jpg', 'image/png']
+
+// Keep these in sync with the backend options in
+// core.models.UserProfile.EmailNotificationFrequencyOptions
 export const EMAIL_NOTIFICATIONS_FREQUENCY_OPTIONS = {
   INSTANT: 'instant',
   DAILY: 'daily',
